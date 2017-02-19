455739958290f23337360debff72b4b34977e67c
Maksym Shustov <maksym_shustov@epam.com>
2017-02-19 07:56:47 +0200

    Add project files.

---
+++ WhatsSay.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace WhatsSay.Droid
+{
+    [Activity(Label = "WhatsSay.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
