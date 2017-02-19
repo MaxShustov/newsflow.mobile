eb1fcbed46e736d15411698b73314d9e1ef03aa3
Maksym Shustov <maksym_shustov@epam.com>
2017-02-19 12:06:12 +0200

    Add test UI

--- WhatsSay.Droid/MainActivity.cs
+++ WhatsSay.Droid/MainActivity.cs
@@ -1,18 +1,41 @@
-﻿using Android.App;
-using Android.Widget;
+﻿using System;
+using Android.App;
 using Android.OS;
+using Android.Widget;
+using WhatsSay.Core.ApiClients.Implementation;
 
 namespace WhatsSay.Droid
 {
     [Activity(Label = "WhatsSay.Droid", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private EditText _userNameEditText;
+        private EditText _passwordEditText;
+        private Button _loginButton;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView(Resource.Layout.Main);
+
+            FindIds();
+
+            _loginButton.Click += LoginButtonOnClick;
+        }
+
+        private async void LoginButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            var apiContext = new ApiContext();
+
+            var token = await apiContext.Login(_userNameEditText.Text, _passwordEditText.Text);
+        }
+
+        private void FindIds()
+        {
+            _userNameEditText = FindViewById<EditText>(Resource.Id.UserNameEditText);
+            _passwordEditText = FindViewById<EditText>(Resource.Id.PasswordEditText);
+            _loginButton = FindViewById<Button>(Resource.Id.LoginButton);
         }
     }
 }
--- WhatsSay.Droid/Resources/Resource.Designer.cs
+++ WhatsSay.Droid/Resources/Resource.Designer.cs
@@ -57,6 +57,28 @@ namespace WhatsSay.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050002
+			public const int LoginButton = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int PasswordEditText = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int UserNameEditText = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
